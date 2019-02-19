5072214fb56d6463e9c43b871ce975760698ec7e
valerykantor <valery.kantor@gmail.com>
2019-02-19 22:09:04 +0200

    TODO

--- gateway/src/main/java/com/comncon/downtime/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/comncon/downtime/gateway/GatewayApplication.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 @SpringBootApplication
 public class GatewayApplication {
 
+    // TODO: Move API gate way configuration here from yml-file.
     /*@Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
